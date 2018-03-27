b908d09559f34032725487d5a3a4cf6e41abd91d
Travis CI <travis@travis.ci>
2018-03-27 23:21:36 +0000

    Update rendered version to e13e4fe6cbdbb1ab03ed8ce9e29f1a51c93b251f

--- docs/conf.py
+++ docs/conf.py
@@ -112,13 +112,13 @@ release = package.__version__
 # name of a builtin theme or the name of a custom theme in html_theme_path.
 #html_theme = None
 
+
 # Please update these texts to match the name of your package.
 html_theme_options = {
     'logotext1': 'package',  # white,  semi-bold
     'logotext2': '-template',  # orange, light
     'logotext3': ':docs'   # white,  light
     }
-
 
 
 # Custom sidebar templates, maps document names to template names.
